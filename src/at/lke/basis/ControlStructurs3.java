8a0ef4f162479df1a62adc2bda8c648550ac2a69
kevinlimmen <kevin.limmen@student.htldornbirn.at>
2023-10-18 11:19:52 +0200

    Readme File erstellt zum 2-ten

--- src/at/lke/basis/ControlStructurs3.java
+++ src/at/lke/basis/ControlStructurs3.java
@@ -1,20 +1,26 @@
 package at.lke.basis;
-
-        import java.util.Random;
+import java.util.Random;
 
 public class ControlStructurs3 {
     public static void main(String[] args) {
-        // Erstelle eine Zufallszahl zwischen 0 und 100
+        // Erstellet eine Zufallszahl zwischen 0 und 100
         Random random = new Random();
         int randomNumber = random.nextInt(100);
 
-        // Gib die Zufallszahl aus
+        // Gibt die Zufallszahl aus
+        System.out.println("Die Zufallszahl ist: " + randomNumber);
 
-        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
-        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
-        // Wenn die Zahl größer als 50 ist gib aus "Large"
-
-
+        // Wenn die Zahl kleiner ist als 20, gibt es "Mini" aus
+        if (randomNumber < 20) {
+            System.out.println("Mini");
+        }
+        // Wenn die Zahl zwischen 20 und 50 liegt, gib aus "Medium"
+        else if (randomNumber >= 20 && randomNumber <= 50) {
+            System.out.println("Medium");
+        }
+        // Wenn die Zahl größer als 50 ist, gib aus "Large"
+        else {
+            System.out.println("Large");
+        }
     }
 }
-
